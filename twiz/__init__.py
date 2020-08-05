868eac7dcd5eee0797240959838caf6ec938b1b6
Anjan Roy <anjanroy@yandex.com>
2020-08-05 12:46:25 +0530

    added simple package intro

---
+++ twiz/__init__.py
@@ -0,0 +1,11 @@
+'''
+    twiz - Your Twitter Account Data Analysis & Visualization Tool <3
+
+    twiz is one simple CLI based tool for analyzing & visualizing your twitter
+    account's exported data. All computations performed on your machine, designed to
+    work in platform agnostic way.
+
+    Author: Anjan Roy <anjanroy@yandex.com>
+    Github: https://github.com/itzmeanjan
+'''
+__version__ = '0.1.0'
