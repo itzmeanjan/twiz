728294daee4803c5704a1dcc4049ca7228ca6804
Anjan Roy <anjanroy@yandex.com>
2020-08-10 21:02:10 +0530

    extracts compressed twitter account data file into sink directory

--- twiz/extract.py
+++ twiz/extract.py
@@ -2,9 +2,10 @@
 
 from os import mkdir
 from os.path import exists
+from zipfile import ZipFile, BadZipFile
 
 
-def makeDir(target: str) -> bool:
+def makeDirectory(target: str) -> bool:
     '''
         Creates non-existing directory & denotes boolean status
     '''
@@ -13,13 +14,26 @@ def makeDir(target: str) -> bool:
 
     try:
         mkdir(target)
+
         return True
     except OSError:
         return False
 
 
-def extract():
-    pass
+def extract(src: str, sink: str) -> bool:
+    '''
+        Extracts compressed twitter account data file into sink directory
+    '''
+    if not (exists(src) and src.endswith('.zip') and makeDirectory(sink)):
+        return False
+
+    try:
+        _zf = ZipFile(src)
+        _zf.extractall(sink)
+
+        return True
+    except BadZipFile:
+        return False
 
 
 if __name__ == '__main__':
