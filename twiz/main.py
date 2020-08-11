d8c2e8d5c16a147d5e8477462fc47de4867a38fb
Anjan Roy <anjanroy@yandex.com>
2020-08-11 08:42:34 +0530

    parse cmd args

---
+++ twiz/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from argparse import ArgumentParser
+from typing import Tuple
+from os.path import exists, abspath
+
+
+def _getCMD() -> Tuple[str, str]:
+    parser = ArgumentParser(
+        prog='twiz',
+        description='twiz - Your Twitter Account Data Analysis & Visualization Tool <3')
+    parser.add_argument('src', type=str, nargs=1,
+                        help='Compressed File with Twitter Account Data')
+    parser.add_argument('sink', type=str, nargs=1,
+                        help='Zip extraction directory')
+    args = parser.parse_args()
+
+    return (abspath(args.src[0]), abspath(args.sink[0]))
+
+
+def main():
+    try:
+        src, sink = _getCMD()
+        if not exists(src):
+            raise RuntimeError('Source file absent')
+
+    except KeyboardInterrupt:
+        print('\n[!]Terminated')
+    except Exception as e:
+        print('[!]Error: {}'.format(e))
+
+
+if __name__ == '__main__':
+    main()
