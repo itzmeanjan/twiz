8127cc6acdc60a0b63aaa0323b0efea90ca44aa2
Anjan Roy <anjanroy@yandex.com>
2020-08-28 11:23:55 +0530

    reorganized class structure

--- twiz/model/ad/engagement/engagement.py
+++ twiz/model/ad/engagement/engagement.py
@@ -3,17 +3,23 @@
 from .device import DeviceInfo
 from .tweet import Tweet
 from .advertiser import Info
-from .criterias import MatchedCriterias
+from .criteria import MatchedCriteria
 from .attributes import EngagementAttributes
+from datetime import datetime
+from typing import List
 
 
 class Engagement:
-    def __init__(self, device: DeviceInfo, tweet: Tweet, advertiser: Info, criterias: MatchedCriterias, attr: EngagementAttributes):
+    def __init__(self, device: DeviceInfo, location: str, tweet: Tweet,
+                 advertiser: Info, criterias: List[MatchedCriteria],
+                 time: str, attrs: List[EngagementAttributes]):
         self.device = device
+        self.displayLocation = location
         self.tweet = tweet
         self.advertiser = advertiser
         self.criterias = criterias
-        self.attr = attr
+        self.time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+        self.attrs = attrs
 
 
 if __name__ == '__main__':
