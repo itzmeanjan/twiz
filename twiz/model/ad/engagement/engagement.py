2790301ac6dbfac1ff236e93bdebe2c83675c123
Anjan Roy <anjanroy@yandex.com>
2020-08-28 19:00:40 +0530

    wrote static method building engagement obj

--- twiz/model/ad/engagement/engagement.py
+++ twiz/model/ad/engagement/engagement.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+from __future__ import annotations
 from .device import DeviceInfo
 from .tweet import Tweet
 from .advertiser import Info
 from .criteria import MatchedCriteria
 from .attributes import EngagementAttributes
 from datetime import datetime
-from typing import List
+from typing import List, Dict, Any
 
 
 class Engagement:
@@ -20,6 +21,32 @@ class Engagement:
         self.criterias = criterias
         self.time = datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
         self.attrs = attrs
+
+    @staticmethod
+    def build(data: Dict[str, Any]) -> Engagement:
+        impressionAttr = data['impressionAttributes']
+        engagementAttr = data['engagementAttributes']
+
+        return Engagement(
+            DeviceInfo(impressionAttr['deviceInfo']['osType'],
+                       impressionAttr['deviceInfo']['deviceId']),
+            impressionAttr['displayLocation'],
+            Tweet(
+                impressionAttr['promotedTweetInfo']['tweetId'],
+                impressionAttr['promotedTweetInfo']['tweetText'],
+                impressionAttr['promotedTweetInfo']['urls'],
+                impressionAttr['promotedTweetInfo']['mediaUrls']
+            ),
+            Info(
+                impressionAttr['advertiserInfo']['advertiserName'],
+                impressionAttr['advertiserInfo']['screenName']
+            ),
+            [MatchedCriteria(i['targetingType'], i['targetingValue'])
+             for i in impressionAttr['matchedTargetingCriteria']],
+            impressionAttr['impressionTime'],
+            [EngagementAttributes(i['engagementTime'], i['engagementType'])
+             for i in engagementAttr]
+        )
 
 
 if __name__ == '__main__':
