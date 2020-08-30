ded0b0bdcd59ab372482acec4d4e1dc87285b38e
Anjan Roy <anjanroy@yandex.com>
2020-08-30 10:15:51 +0530

    group ad count by device type & display location of ad

--- twiz/model/ad/engagement/engagements.py
+++ twiz/model/ad/engagement/engagements.py
@@ -5,6 +5,7 @@ from .engagement import Engagement
 from typing import List, Dict, Any, Tuple
 from itertools import chain
 from collections import Counter
+from functools import reduce
 
 
 class Engagements:
@@ -41,6 +42,25 @@ class Engagements:
         return [(i, (_buffer[i] / _total) * 100) for i in sorted(_buffer,
                                                                  key=lambda e: _buffer[e],
                                                                  reverse=True)]
+
+    def groupAdCountByDeviceTypeAndDisplayLocation(self) -> Dict[str, Counter]:
+        '''
+            Distribution of shown twitter ads by device type & respective location
+            of display on device
+        '''
+        def _groupByDeviceType(acc: Dict[str, List[str]], cur: Tuple[str, str]) -> Dict[str, List[str]]:
+            if cur[0] in acc:
+                acc[cur[0]].append(cur[1])
+            else:
+                acc[cur[0]] = [cur[1]]
+
+            return acc
+
+        return dict(map(lambda e: (e[0], Counter(e[1])),
+                        reduce(_groupByDeviceType,
+                               map(lambda e: (e.device.type, e.displayLocation),
+                                   self.all),
+                               {}).items()))
 
 
 if __name__ == '__main__':
