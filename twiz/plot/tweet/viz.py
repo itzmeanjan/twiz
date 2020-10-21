07298834541a617d68a9c4ef4d29affe6efcbbc7
Anjan Roy <anjanroy@yandex.com>
2020-10-21 09:00:36 +0530

    plotting top X user mentions in tweets by USER

--- twiz/plot/tweet/viz.py
+++ twiz/plot/tweet/viz.py
@@ -8,13 +8,35 @@ from twiz.model.tweet.tweets import Tweets
 def plotTopXHashTags(data: Tweets, x: int, title: str, sink: str):
     try:
         _tmp = data.topXHashTagsWithCount(x)
-        x = [i[0] for i in _tmp]
-        y = [i[1] for i in _tmp]
+        _x = [i[0] for i in _tmp]
+        _y = [i[1] for i in _tmp]
 
         fig = plt.Figure(figsize=(16, 9), dpi=100)
         sns.set_style('darkgrid')
 
-        sns.barplot(x=y, y=x, ax=fig.gca(), orient='h', palette='plasma')
+        sns.barplot(x=_y, y=_x, ax=fig.gca(), orient='h', palette='plasma')
+
+        fig.gca().set_title(title, fontsize=20, pad=10)
+        fig.tight_layout(pad=4)
+
+        fig.savefig(sink, pad_inches=.8)
+        plt.close(fig)
+
+        return True
+    except Exception:
+        return False
+
+
+def plotTopXUserMentions(data: Tweets, x: int, title: str, sink: str):
+    try:
+        _tmp = data.topXUserMentionsWithCount(x)
+        _x = [i[0] for i in _tmp]
+        _y = [i[1] for i in _tmp]
+
+        fig = plt.Figure(figsize=(16, 9), dpi=100)
+        sns.set_style('darkgrid')
+
+        sns.barplot(x=_y, y=_x, ax=fig.gca(), orient='h', palette='plasma')
 
         fig.gca().set_title(title, fontsize=20, pad=10)
         fig.tight_layout(pad=4)
